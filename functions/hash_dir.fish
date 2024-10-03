30aab9ae4f0299c45809c7c56ac5601470950bbf
Kuba86 <iqvr35my@duck.com>
2024-10-03 23:50:18 +0200

    Update hash_dir.fish
    return only hash without '-'

--- functions/hash_dir.fish
+++ functions/hash_dir.fish
@@ -1,3 +1,3 @@
 function hash_dir --description 'creates sha1 hash of a directory. Takes a single argument, the directory'
-  find $argv[1] -type f -print0 | sort -zdf | xargs -0 sha1sum | awk '{print $1}' | sha1sum
+  find $argv[1] -type f -print0 | sort -zdf | xargs -0 sha1sum | awk '{print $1}' | sha1sum | awk '{print $1}'
 end
