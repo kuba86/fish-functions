2fa8db22a3b7bb04e4522edf14b1207dff8005b8
kuba86 <8183159+kuba86@users.noreply.github.com>
2024-10-13 14:48:50 +0200

    sanitize from to and msg variables and silent curl output

--- functions/signal-api-send.fish
+++ functions/signal-api-send.fish
@@ -2,21 +2,25 @@ function signal-api-send
   argparse 'from=' 'to=' 'msg=' -- $argv
   or return
 
+  set -g from $(printf "%s" "$_flag_from" | jq -R -s -c)
+  set -g to $(printf "%s" "$_flag_to" | jq -R -s -c)
+  set -g msg $(printf "%s" "$_flag_msg" | jq -R -s -c)
+
   if not set -q _flag_from || not set -q _flag_to || not set -q _flag_msg
       echo "ERROR: some flag were not set. from= to= msg= are required"
       return 1
   end
 
-  set -g json $(string join '' '{"message":' $_flag_msg ',"number":' $_flag_from ',"recipients":[' $_flag_to ']}')
+  set -g json $(string join '' '{"message":' $msg ',"number":' $from ',"recipients":[' $to ']}')
 
-  echo "signal-api-send from: $_flag_from"
-  echo "signal-api-send to: $_flag_to"
-  echo "signal-api-send msg: $_flag_msg"
+  echo "signal-api-send from: $from"
+  echo "signal-api-send to: $to"
+  echo "signal-api-send msg: $msg"
   echo "signal-api-send remaining argv: $argv"
   echo "signal-api-send json: $json"
 
   podman exec signal-api \
-    curl \
+    curl --silent \
     -X POST \
     -H "Content-Type: application/json" \
     "http://localhost:8080/v2/send" \
