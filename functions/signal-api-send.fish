71421ce210135c3a0369a1d54a03fcb8d1b0c584
kuba86 <8183159+kuba86@users.noreply.github.com>
2024-10-13 14:51:44 +0200

    reorder code

--- functions/signal-api-send.fish
+++ functions/signal-api-send.fish
@@ -5,12 +5,6 @@ function signal-api-send
   set -g from $(printf "%s" "$_flag_from" | jq -R -s -c)
   set -g to $(printf "%s" "$_flag_to" | jq -R -s -c)
   set -g msg $(printf "%s" "$_flag_msg" | jq -R -s -c)
-
-  if not set -q _flag_from || not set -q _flag_to || not set -q _flag_msg
-      echo "ERROR: some flag were not set. from= to= msg= are required"
-      return 1
-  end
-
   set -g json $(string join '' '{"message":' $msg ',"number":' $from ',"recipients":[' $to ']}')
 
   echo "signal-api-send from: $from"
@@ -18,6 +12,11 @@ function signal-api-send
   echo "signal-api-send msg: $msg"
   echo "signal-api-send remaining argv: $argv"
   echo "signal-api-send json: $json"
+
+  if not set -q _flag_from || not set -q _flag_to || not set -q _flag_msg
+      echo "ERROR: some flag were not set. from= to= msg= are required"
+      return 1
+  end
 
   podman exec signal-api \
     curl --silent \
